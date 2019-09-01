6ccf23cebc5ba453f9a23cb89f79b0e2352dc7b4
Fabiano <fabiano.paula.martins@gmail.com>
2019-09-01 11:37:41 -0300

    Adding a HTTPS connection on application

--- backend/libs/boot.js
+++ backend/libs/boot.js
@@ -1,8 +1,15 @@
+import https from "https";
+import fs from "fs";
+
 module.exports = app => {
   if (process.env.NODE_ENV !== "test") {
+    const credentials = {
+      key: fs.readFileSync("ntask.key","utf8"),
+      cert: fs.readFileSync("ntask.cert","utf8")
+    }
     app.db
       .sequelize.sync({ underscored: true })
-      .then(() => app.listen(app.set("port")))
+      .then(() => https.createServer(credentials,app).listen(app.set("port")))
       .then(() => console.log(`NTask API - porta ${app.set("port")}`) );
   }else{
     app.db.sequelize.sync({ underscored: true })
